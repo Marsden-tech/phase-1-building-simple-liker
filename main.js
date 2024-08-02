095e4ff4075fa12349433ef7708080f7a4a34d14
Marsden Mathews Odhiambo <marsdenlwero@gmail.com>
2024-08-02 10:32:03 +0300

    completes lab

--- main.js
+++ main.js
@@ -3,7 +3,31 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const articleLikers = document.querySelectorAll(".like-glyph");
 
+function showLike(event) {
+  const liker = event.target
+  mimicServerCall()
+    .then(() => {
+      if (liker.innerText === EMPTY_HEART) {
+        liker.innerText = FULL_HEART
+        liker.className = 'activated-heart'
+      }
+      else {
+        liker.innerText = EMPTY_HEART
+        liker.className = ''
+      }
+    }).catch((err) => {
+      const modal = document.getElementById('modal')
+      modal.className = ''
+      modal.innerText = err
+      setTimeout(() => modal.className = 'hidden', 3000)
+    })
+}
+
+for (const like of articleLikers) {
+  like.addEventListener('click', showLike)
+}
 
 
 
@@ -11,9 +35,9 @@ const FULL_HEART = '♥'
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
       let isRandomFailure = Math.random() < .2
       if (isRandomFailure) {
         reject("Random server error. Try again.");
